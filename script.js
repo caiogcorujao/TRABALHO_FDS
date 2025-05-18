b0ebf1b557b133cfa3b6ae8b60dc2013a53cc075
Caio Santos <83424629+caiogcorujao@users.noreply.github.com>
2025-05-18 18:38:13 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+function mostrarMensagem() {
+    const divMensagem = document.getElementById("mensagem");
+    divMensagem.innerHTML = "<p><strong>Você pode brilhar, não importa do que seja feito!</strong></p>";
+  }
+  
